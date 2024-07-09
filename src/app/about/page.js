19423885e26cebf0909a6f2f6ccaf6a37b443142
israrahmedjan <israrahmedjan@gmail.com>
2024-07-09 19:40:00 +0500

    data saved

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,26 +10,8 @@ export default async function page() {
         <div className="">
           <h2 className="text-2xl text-customRed">About Us</h2>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged.It was popularised in the 1960s with
-            the release of Letraset sheets containing Lorem Ipsum passages, and
-            more recently with desktop publishing software like Aldus PageMaker
-            including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of add items the printing and
-            typesetting industry. Lorem Ipsum has been the industry's standard
-            dummy text ever since the 1500s, when an unknown printer took a
-            galley of type and scrambled it to make a type specimen book. It has
-            survived not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged.It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
+            daksdfj skdfjsdkj sdkfjskdjf ksfjsjdf skfjskdj skdfjskj skfskjf
+            skfjskjf ksfjsjdf ksfjskj skdfjskdjf
           </p>
         </div>
       </div>
