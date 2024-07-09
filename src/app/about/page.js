243c6cc78cf9fd6c834c6e64a987c6bc985ac28d
israrahmedjan <israrahmedjan@gmail.com>
2024-07-09 20:01:38 +0500

    About page changed

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,8 +10,17 @@ export default async function page() {
         <div className="">
           <h2 className="text-2xl text-customRed">About Us</h2>
           <p>
-            daksdfj skdfjsdkj sdkfjskdjf ksfjsjdf skfjskdj skdfjskj skfskjf
-            skfjskjf ksfjsjdf ksfjskj skdfjskdjf
+            Our company consists of an expert team of professionals who are
+            knowledgeable on the process of developmental projects in Abu Dhabi.
+            We are known to be the company that creates innovative and
+            sustainable solutions even to the most challenging project. We
+            provide every engineering solution needed for the projects of our
+            clients and assist them through a smooth and hassle-free procedure
+            to acquire approvals from DMT, ADCD, ADM, Al Ain Municipality, Al
+            Dhafrah Municipality, ITC, DOH, Food Control and Safety, ADDC, DCT,
+            Masdar City, KIZAD, Industrial City, and many more. The accuracy of
+            our services and the creativity of our team provide our clients the
+            best experience with us.gi
           </p>
         </div>
       </div>
