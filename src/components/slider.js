5c82e6a6778005053a70ddd140bdc0dd501d4ce9
israrahmedjan <israrahmedjan@gmail.com>
2024-07-10 21:25:13 +0500

    Button added

--- src/components/slider.js
+++ src/components/slider.js
@@ -38,8 +38,8 @@ export default function Slider() {
                   className="h-16 w-[350px] p-3 border border-gray-500"
                   placeholder="Search of Services"
                 />
-                <h1 className="text-white text-4xl font-bold">
-                  Welcome to My Website
+                <h1 className="text-white text-4xl font-bold uppercase">
+                  RESIDENTIAL APPROVAL
                 </h1>
                 <p>
                   A home should always possess comfortable and functional
@@ -49,23 +49,26 @@ export default function Slider() {
                   design in Abu Dhabi. Our expert team will help you give shape
                   and build your dream home.
                 </p>
+                <button className="bg-customRed text-white py-4 px-3 w-[200px] font-bold uppercase">
+                  Book Service
+                </button>
               </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <img
-              src="/images/banner.jpg"
+              src="/images/banner-2.jpg"
               style={{ width: "100%", height: "550px", objectFit: "cover" }}
             />
             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-start">
               <div className="flex-2 flex flex-col w-[700px] items-stretch px-24 space-y-8">
                 <input
                   type="text"
-                  className="h-16 border border-gray-500"
+                  className="h-16 w-[350px] p-3 border border-gray-500"
                   placeholder="Search of Services"
                 />
-                <h1 className="text-white text-4xl font-bold">
-                  Welcome to My Website
+                <h1 className="text-white text-4xl font-bold uppercase">
+                  Search for Services OUR ABU DHABI SERVICES
                 </h1>
                 <p>
                   A home should always possess comfortable and functional
@@ -75,6 +78,38 @@ export default function Slider() {
                   design in Abu Dhabi. Our expert team will help you give shape
                   and build your dream home.
                 </p>
+                <button className="bg-customRed text-white py-4 px-3 w-[200px] font-bold uppercase">
+                  Book Service
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="/images/banner-2.jpg"
+              style={{ width: "100%", height: "550px", objectFit: "cover" }}
+            />
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-start">
+              <div className="flex-2 flex flex-col w-[700px] items-stretch px-24 space-y-8">
+                <input
+                  type="text"
+                  className="h-16 w-[350px] p-3 border border-gray-500"
+                  placeholder="Search of Services"
+                />
+                <h1 className="text-white text-4xl font-bold uppercase">
+                  Search for Services OUR ABU DHABI SERVICES
+                </h1>
+                <p>
+                  A home should always possess comfortable and functional
+                  features regardless of how grand or simple the style is. We
+                  provide architectural and engineering assistance to get
+                  approvals for your residential projects especially villa
+                  design in Abu Dhabi. Our expert team will help you give shape
+                  and build your dream home.
+                </p>
+                <button className="bg-customRed text-white py-4 px-3 w-[200px] font-bold uppercase">
+                  Book Service
+                </button>
               </div>
             </div>
           </SwiperSlide>
